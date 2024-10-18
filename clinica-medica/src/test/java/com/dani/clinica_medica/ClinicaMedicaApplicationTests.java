db7065163e814d94de2cd15778a18eacb1e69b72
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-10-18 15:10:01 -0300

    Starting the project

---
+++ clinica-medica/src/test/java/com/dani/clinica_medica/ClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dani.clinica_medica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
