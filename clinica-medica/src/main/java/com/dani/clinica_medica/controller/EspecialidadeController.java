29618a5b84be547325afb2e5585453559a2732af
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-10-21 21:44:28 -0300

    adding get, put, delete in the class EspecialidadeController

--- clinica-medica/src/main/java/com/dani/clinica_medica/controller/EspecialidadeController.java
+++ clinica-medica/src/main/java/com/dani/clinica_medica/controller/EspecialidadeController.java
@@ -4,7 +4,10 @@ package com.dani.clinica_medica.controller;
 import com.dani.clinica_medica.dao.EspecialidadeDao;
 import com.dani.clinica_medica.entity.Especialidade;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("especialidades")
@@ -14,9 +17,35 @@ public class EspecialidadeController {
     private EspecialidadeDao especialidadeDao;
 
     @PostMapping
-    public Especialidade salvarEspecialidade(@RequestBody Especialidade
-                                                         especialidade){
+    public Especialidade salvarEspecialidade(@RequestBody Especialidade especialidade) {
         especialidadeDao.save(especialidade);
         return especialidade;
+    }
+
+    @PutMapping
+    public Especialidade atualizarEspecialidade(@RequestBody Especialidade especialidade) {
+        especialidadeDao.update(especialidade);
+        return especialidade;
+    }
+
+    @DeleteMapping("{id}")
+    public String deletarEspecialidade(@PathVariable Long id) {
+        especialidadeDao.delete(id);
+        try {
+            return "Especialiade id: " + id + " deletado com sucesso";
+        } catch (Exception e) {
+            // parei aqui, mostra o erro no postman mas não cai aqui no catch
+            return "Especialidade vinculada com cadastro de médico, não é " + "possivel deletar";
+        }
+    }
+
+    @GetMapping
+    public List<Especialidade> buscarTodasEspecialidades() {
+        return especialidadeDao.findAll();
+    }
+
+    @GetMapping("{descricao}")
+    public Especialidade buscarPelaDescricao(@PathVariable String descricao) {
+        return especialidadeDao.findByDescricao(descricao);
     }
 }
--- clinica-medica/src/main/java/com/dani/clinica_medica/dao/EspecialidadeDao.java
+++ clinica-medica/src/main/java/com/dani/clinica_medica/dao/EspecialidadeDao.java
@@ -5,6 +5,8 @@ import jakarta.persistence.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Repository
 public class EspecialidadeDao {
 
@@ -14,5 +16,34 @@ public class EspecialidadeDao {
     @Transactional(readOnly = false)
     public void save (Especialidade especialidade){
         this.entityManager.persist(especialidade);
+    }
+
+    @Transactional
+    public void update (Especialidade especialidade) {
+        this.entityManager.merge(especialidade);
+    }
+
+    @Transactional
+    public void delete (Long id){
+        this.entityManager.remove(this.entityManager.getReference(Especialidade.class, id));
+    }
+
+    @Transactional
+    public List<Especialidade> findAll(){
+        String query = "select especialidade from Especialidade especialidade";
+        return this.entityManager.createQuery(query, Especialidade.class).getResultList();
+    }
+
+    @Transactional(readOnly = true)
+    public Especialidade findById(Long id){
+        return this.entityManager.find(Especialidade.class, id);
+    }
+
+    @Transactional(readOnly = true)
+    public Especialidade findByDescricao (String descricao) {
+        String query = """
+                select e from Especialidade e where e.descricao like :descricao
+                """;
+        return this.entityManager.createQuery(query, Especialidade.class).setParameter("descricao",descricao).getSingleResult();
     }
 }
