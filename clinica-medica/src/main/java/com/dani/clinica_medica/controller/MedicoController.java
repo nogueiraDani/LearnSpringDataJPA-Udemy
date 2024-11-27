5894babaf55076458cb0cf2d1adce0a170f69dda
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-11-27 14:59:01 -0300

    :recycle: update imports

--- clinica-medica/src/main/java/com/dani/clinica_medica/controller/MedicoController.java
+++ clinica-medica/src/main/java/com/dani/clinica_medica/controller/MedicoController.java
@@ -9,10 +9,7 @@ import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
@@ -55,6 +52,5 @@ public class MedicoController {
 
         medicoDao.save(medico);
         return ResponseEntity.ok(medico);
-
     }
 }
